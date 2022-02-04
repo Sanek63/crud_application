a702fd81d1c71c91540aac4165b6a9a27dea7c7e
Alexandr Shabalkin <sashatlt8@gmail.com>
2022-02-04 09:50:19 +0400

    minor fixes

--- backend/app/app/api/api_v1/endpoints/statistic.py
+++ backend/app/app/api/api_v1/endpoints/statistic.py
@@ -6,15 +6,12 @@ from app import crud, models, schemas # noqa
 class StatisticView(BaseHandler):
     def get(self):
         with session_scope() as db:
-            duplicate_elements = crud.msg.get_duplicate_elements(db=db)
-            single_elements = crud.msg.get_single_elements(db=db)
-
-            duplicate_counter_sum = sum([el.counter for el in duplicate_elements]) if duplicate_elements else 0
-            single_counter_sum = sum([el.counter for el in single_elements]) if single_elements else 0
+            duplicate_counter_sum = crud.msg.get_duplicate_counter_sum(db=db)
+            single_counter_sum = crud.msg.get_single_counter_sum(db=db)
 
         total = duplicate_counter_sum + single_counter_sum
 
-        duplicate_percent = duplicate_counter_sum / total if total else 0
+        duplicate_percent = duplicate_counter_sum / total if total > 0 else 0
         return self.write(
             {
                 "percent": int(round(duplicate_percent, 2) * 100)
--- backend/app/app/crud/msg.py
+++ backend/app/app/crud/msg.py
@@ -4,6 +4,8 @@ from app.crud.base import CRUDBase # noqa
 from app.db.session import Session # noqa
 from app.models import Msg # noqa
 
+from sqlalchemy.sql import func
+
 
 class CRUDMsg(CRUDBase):
     def get_element_by_key(self, db: Session, *, key: str) -> Optional[Msg]:
@@ -14,5 +16,23 @@ class CRUDMsg(CRUDBase):
 
     def get_single_elements(self, db: Session) -> List[Msg]:
         return db.query(self.model).filter(self.model.counter == 1).all()
+
+    def get_duplicate_counter_sum(self, db: Session) -> int:
+        counter_sum = db.query(
+            func.sum(self.model.counter)
+        ).filter(
+            self.model.counter > 1
+        ).scalar()
+
+        return counter_sum if counter_sum else 0
+
+    def get_single_counter_sum(self, db: Session) -> int:
+        counter_sum = db.query(
+            func.sum(self.model.counter)
+        ).filter(
+            self.model.counter == 1
+        ).scalar()
+
+        return counter_sum if counter_sum else 0
 
 crud_msg = CRUDMsg(Msg)
